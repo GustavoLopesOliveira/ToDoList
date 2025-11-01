71a32fa7fe8c2dc8d420969cdd7ddb3fbc3f2759
Gustavo Lopes <gustavolopesoliveira285@gmail.com>
2025-10-31 22:16:27 -0300

    fix: baseModel

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,46 @@
+from fastapi import FastAPI
+
 from model.Task import Task
 
+app = FastAPI()
 
+
+
+datas = [
+    Task(id=1, titulo="aa", descricao="aaa")
+]
+
+@app.get("/")
+def getTaskAll():
+    return datas
+
+@app.get("/{id}")
+def getTaskById(id: int):
+    for data in datas:
+        if data.id == id:
+            return data
+    return {"mensagem": "Tarefa não encontrada"}
+
+@app.post("/")
+def postTask(task: Task):
+    datas.append(task)
+    return {"mensagem": "Tarefa adicionada com sucesso"}
+
+@app.put("/{id}")
+def putTask(id: int, task: Task):
+    for i, data in enumerate(datas):
+        if data.id == id:
+            datas[i] = task
+            return {"mensagem": "Tarefa atualizada com sucesso"}
+    return {"mensagem": "Tarefa não encontrada"}
+
+@app.delete("/")
+def deleteAll():
+    datas.clear()
+    return {"mensagem": "Todas as tarefas foram deletadas"}
+
+@app.delete("/{id}")
+def deleteTask(id: int):
+    global datas
+    datas = [data for data in datas if data.id != id]
+    return {"mensagem": f"Tarefa com id={id} deletada"}
--- backend/model/Task.py
+++ backend/model/Task.py
@@ -1,25 +1,6 @@
-class Task:
+from pydantic import BaseModel
 
-    def __init__(self,id : int,name : str,description : str):
-        self.id = id
-        self.name = name
-        self.description = description
-    
-    def getName(self):
-        return self.name
-    
-    def getDescription(self):
-        return self.description
-    
-    def getId(self):
-        return self.id
-    
-    def setName(self,name : str):
-        self.name = name
-    
-    def setDescription(self, description : str):
-        self.description = description
-    
-    def setId(self, id : int):
-        self.id = id
-
+class Task(BaseModel):
+    id: int
+    name: str
+    description: str
